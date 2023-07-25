41a3f4c31c672150600be1a03ced0c26b7c06210
Andrew Ferrazzutti <andrewf@element.io>
2023-07-25 11:35:32 -0400

    Satisfy linter

--- src/types.rs
+++ src/types.rs
@@ -4,7 +4,6 @@ use matrix_sdk_crypto::backups::{
     SignatureState as InnerSignatureState, SignatureVerification as InnerSignatureVerification,
 };
 use napi::bindgen_prelude::{FromNapiValue, ToNapiValue};
-
 use napi_derive::*;
 
 use crate::{
