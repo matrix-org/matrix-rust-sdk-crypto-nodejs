64fe7d72c2dda2c2be691a6db401f3ff6325b4ca
Andrew Ferrazzutti <andrewf@element.io>
2023-09-05 11:45:23 -0400

    Satisfy cargo fmt

--- src/machine.rs
+++ src/machine.rs
@@ -602,14 +602,24 @@ impl OlmMachine {
         }
     }
 
-    /// Export room keys in unencrypted format for a given session_id. This currently exports a 
-    /// json blob.
+    /// Export room keys in unencrypted format for a given session_id.
+    /// This currently exports a json blob.
     #[napi]
-    pub async fn export_room_keys_for_session(&self, room_id: String, session_id: String) -> napi::Result<String> {
+    pub async fn export_room_keys_for_session(
+        &self,
+        room_id: String,
+        session_id: String,
+    ) -> napi::Result<String> {
         serde_json::to_string(
-            &self.inner.export_room_keys(|session| session.session_id() == session_id && session.room_id().eq(&room_id))
-            .await.map_err(into_err)?
-        ).map_err(into_err)
+            &self
+                .inner
+                .export_room_keys(|session| {
+                    session.session_id() == session_id && session.room_id().eq(&room_id)
+                })
+                .await
+                .map_err(into_err)?,
+        )
+        .map_err(into_err)
     }
 
     /// Get the number of backed up room keys and the total number of room keys.
